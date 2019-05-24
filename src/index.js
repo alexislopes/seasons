77b8bc92a7137609ad5be02c02c1fef497be7283
alexislopes <alexisdrops@gmail.com>
2019-05-24 15:37:04 -0300

    exploring the component lifecycle

--- src/index.js
+++ src/index.js
@@ -5,17 +5,35 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {lat: null}
+        //THIS IS ONLY TIME we do redirect assignment
+        // to rhis.state
+        this.state = { lat: null, errorMessage: '' }        
+    }
+
+    componentDidUpdate() {
+        console.log('my component was just updated')
     }
 
     // React says we have to define render!!
     render () {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        )
+        console.log('rendering')
+       if (this.state.errorMessage && !this.state.lat){
+           return <div>Error: {this.state.errorMessage}</div>
+       }
 
-        return <div>Latitude: </div>
+       if (!this.state.errorMessage && this.state.lat) {
+           return <div>Latitude: {this.state.lat}</div>
+       }
+
+       return <div>Loading!</div>
+    }
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState({lat: position.coords.latitude}),
+            (err) => this.setState({ errorMessage: err.message })
+            
+        )
     }
 }
 
